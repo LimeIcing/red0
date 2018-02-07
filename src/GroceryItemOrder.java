aef90373c3ca93c76ccb99f4a9ac03b1bdee73c1
Lime Icing <emil577b@stud.kea.dk>
2018-02-07 14:18:06 +0100

    Added minimal functionality

--- src/GroceryItemOrder.java
+++ src/GroceryItemOrder.java
@@ -1,2 +1,53 @@
 public class GroceryItemOrder {
+    private String name;
+    private int quantity;
+    private double price;
+
+    public GroceryItemOrder(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public GroceryItemOrder(String name) {
+        this.name = name;
+        this.quantity = 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getCost() {
+        return this.price * this.quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "GroceryItemOrder{" +
+                "name='" + this.name + '\'' +
+                ", quantity=" + this.quantity +
+                ", cost=" + this.getCost() +
+                '}';
+    }
 }
--- src/GroceryList.java
+++ src/GroceryList.java
@@ -1,2 +1,32 @@
+import java.util.Arrays;
+
 public class GroceryList {
+    private GroceryItemOrder[] groceryItemOrders = new GroceryItemOrder[10];
+    private int size;
+
+    public void add(GroceryItemOrder item) {
+        if (size >= this.groceryItemOrders.length) {
+            System.out.println("List is full");
+            return;
+        }
+        for (int i = 0; i < this.groceryItemOrders.length; i++) {
+            if (this.groceryItemOrders[i] == null) {
+                this.groceryItemOrders[i] = item;
+                this.size++;
+                return;
+            }
+        }
+    }
+
+    public int getTotalCost() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "GroceryList{" +
+                "groceryItemOrders=" + Arrays.toString(groceryItemOrders) +
+                ", size=" + size +
+                '}';
+    }
 }
